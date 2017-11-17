2f65950408b980a3170f21cbf562cc26079ccb59
yun sunjin <yun_sunjin@cocone.co.jp>
2017-11-17 17:29:55 +0900

    賢治バージョン

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("Gitコネクト");
         System.out.println("バージョン２だお");
+        System.out.println("賢治バージョン");
     }
 }
